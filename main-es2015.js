5b25e41bf5a432809086e946d9b481e52d7123cc
alsia <alisa.ka.60@ubu.ac.th>
2019-09-01 01:07:03 +0700

    add3

--- main-es2015.js
+++ main-es2015.js
@@ -444,7 +444,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_2__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/benz/week9/AlisaBenz-web-source/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /home/benz/Desktop/week9/AlisaBenz-web-source/src/main.ts */"./src/main.ts");
 
 
 /***/ })
